1878828f19479167170b0e1703a4f29791656bf8
OPM <mdamirhossainjowel@gmail.com>
2022-04-09 16:33:01 +0600

    first commit

--- src/Component/Registration/Registration.js
+++ src/Component/Registration/Registration.js
@@ -1,17 +1,24 @@
 import React from "react";
+import useFirebase from "../../Hooks/useFirebase";
 
 const Ragistration = () => {
+  const { handleEmail, handlePassword, createWithEmailandPass } = useFirebase();
   return (
     <div className="text-2xl w-1/3 mt-10 mx-auto">
       <h1 className="text-3xl">Please Register</h1>
       <br />
       <input type="text" placeholder="Your Name" />
       <br /> <br />
-      <input type="email" placeholder="Email Address" />
+      <input onBlur={handleEmail} type="email" placeholder="Email Address" />
       <br /> <br />
-      <input type="email" placeholder="Password" />
+      <input onBlur={handlePassword} type="password" placeholder="Password" />
       <br />
-      <button className="bg-orange-400 mt-12 p-2">Submit</button>
+      <button
+        onClick={createWithEmailandPass}
+        className="bg-orange-400 mt-12 p-2 mr-3"
+      >
+        Submit
+      </button>
     </div>
   );
 };
--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import {
+  createUserWithEmailAndPassword,
   getAuth,
   GoogleAuthProvider,
   onAuthStateChanged,
+  signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
@@ -13,6 +15,8 @@ const auth = getAuth(app);
 const useFirebase = () => {
   const googleProvider = new GoogleAuthProvider();
   const [user, setUser] = useState({});
+  const [email, setEmail] = useState({});
+  const [password, setPassword] = useState({});
   const signWithGoogle = () => {
     signInWithPopup(auth, googleProvider).then((result) => {
       const user = result.user;
@@ -20,18 +24,59 @@ const useFirebase = () => {
       console.log(user);
     });
   };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
   }, []);
+  const createWithEmailandPass = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+  const signWithEmailandPass = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   const SignOut = () => {
     signOut(auth)
       .then(() => {})
       .catch((error) => {});
   };
 
-  return { user, signWithGoogle, SignOut };
+  return {
+    user,
+    signWithGoogle,
+    SignOut,
+    handleEmail,
+    handlePassword,
+    signWithEmailandPass,
+    createWithEmailandPass,
+  };
 };
 
 export default useFirebase;
